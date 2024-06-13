8bd3ab9ffc7b60a370044e34ab8ac505dc3326d1
Nicohim87 <nico.himawan@binus.ac.id>
2024-06-13 14:30:00 +0700

    Minor Bugfix

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -101,7 +101,7 @@
 									<path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
 								</svg>
 							</div>
-							<h4 class="program-subtitle">Program</h4>
+							<h4 class="program-subtitle">Programs</h4>
 						</div>
 						<div class="program-item program-item-red">
 							<div class="program-icon">
@@ -117,7 +117,7 @@
 									<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
 								</svg>
 							</div>
-							<h4 class="program-subtitle">Member</h4>
+							<h4 class="program-subtitle">Gym</h4>
 						</div>
 						<div class="program-item program-item-green">
 							<div class="program-icon">
@@ -125,7 +125,7 @@
 									<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z" />
 								</svg>
 							</div>
-							<h4 class="program-subtitle">BMI</h4>
+							<h4 class="program-subtitle">BMI Calculator</h4>
 						</div>
 					</div>
 				</div>
